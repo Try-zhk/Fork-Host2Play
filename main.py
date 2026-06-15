5fd8c4830a22193b98914883f38a0f6ef485e585
Hongkun Zou <543172183qq@gmail.com>
2026-06-15 21:00:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ except ImportError:
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=9d2daed2-cbd8-4566-ae10-626b20ab1c38",
     "https://host2play.gratis/server/renew?i=4dcf93d1-df50-499d-b258-08249d49ec11",
-    "https://host2play.gratis/server/renew?i=0f185905-ed8d-40c3-a337-4fbe02344327",
     # 添加更多链接
 ]
 
